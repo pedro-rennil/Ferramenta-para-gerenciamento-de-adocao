9b3fa0b92d4139974d05701ea4b63f40535a53a1
Janderson Ferreira <silaferreira.janderson@gmail.com>
2024-10-23 14:06:51 -0300

    função menu criada

---
+++ principal.py
@@ -0,0 +1,11 @@
+import crud
+
+def menu():
+    print("==== Gerenciamento de Adoção de Animais ====")
+    print("1. Listar Animais")
+    print("2. Adicionar Novo Animal")
+    print("3. Atualizar Dados de um Animal")
+    print("4. Remover Animal")
+    print("5. Sair")
+
+       
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+ animais = crud.carregar_dados()
