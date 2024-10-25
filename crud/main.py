f739750e921a7fc7994f4074ddd9548a6f12cf53
Janderson Ferreira <silaferreira.janderson@gmail.com>
2024-10-25 15:10:35 -0300

    Atualizando arquivo main.py

--- crud/main.py
+++ crud/main.py
@@ -1,11 +1,34 @@
-import crud.adoção.operations as operations
+def menu_principal():
+    print("===== CESAR'S ANIMAL HUB =====")
+    print("1. Gerenciamento de Adoção de Animais")
+    print("2. Gerenciamento de Clientes")
+    print("3. Pet Shop")
+    print("4. Sair")
 
-def menu():
-    print("==== Gerenciamento de Adoção de Animais ====")
-    print("1. Listar Animais")
-    print("2. Adicionar Novo Animal")
-    print("3. Atualizar Dados de um Animal")
-    print("4. Remover Animal")
-    print("5. Sair")
+def main():
+    while True:
+        menu_principal()
+        op = input("Escolha a opção desejada: ")
+
+        if op == '1':
+            import adoção.init as adocao
+            adocao.main()
+        elif op == '2':
+            import cliente.init as cliente
+            cliente.main()
+        elif op == '3':
+            import pet_shop.init as pet_shop
+            pet_shop.main()
+        elif op == '4':
+            print("Saindo do sitema... ")
+            break
+        else:
+            print("Opção inválida! Tente novamente. ")
+
+if __name__ == "__main__":
+    main()
+
+
+
 
        
